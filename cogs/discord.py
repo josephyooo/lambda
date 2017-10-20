1285a951d87c0cd2dcd0474b621eaac5296b942c
Polokniko <31323641+Polokniko@users.noreply.github.com>
2017-10-20 18:45:47 -0400

    Added description

--- cogs/discord.py
+++ cogs/discord.py
@@ -7,13 +7,17 @@ class Discord:
     def __init__(self, lambdabot):
         self.lambdabot = lambdabot
 
-    @commands.command(name='joined', aliases=['joinedat'])
+    @commands.command(name='joined', aliases=['joinedat'],
+                      description="A command that will send the date that a certain member has joined.")
     async def joined(self, ctx, member: discord.Member):
+        # `joined <member>
         date = member.joined_at.strftime('%B %d, %Y')
         await ctx.send(f'"{member.name}" joined this server on {date}.')
 
-    @commands.command(name='clear', aliases=['purge'])
+    @commands.command(name='clear',
+                      description="A command that will clear a specified number of messages from a channel.")
     async def clear(self, ctx, amount: int):
+        # `clear <amount>
         amount += 1
         if amount > 30:
             await ctx.send("That's too much. Please use a number below 30")
@@ -22,12 +26,16 @@ class Discord:
             time.sleep(0.5)
             await ctx.send(f'I have cleared {amount - 1} messages.')
 
-    @commands.command(name='id')
+    @commands.command(name='id',
+                      description="A command that will send the given member's id")
     async def id(self, ctx, *, member: discord.Member):
-        await ctx.send(member.id)
+        # `id <member>
+        await ctx.send(f"{member.name}'s id is <{member.id}>")
 
-    @commands.command(name='owner')
+    @commands.command(name='owner',
+                      description="A command that will send the guild's owner.")
     async def owner(self, ctx):
+        # `owner
         await ctx.send(ctx.guild.owner.mention)
 
 
