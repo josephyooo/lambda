68e3c08a91eb2ae1b1006e2983635e11244f7b9c
Joseph <31323641+Polokniko@users.noreply.github.com>
2017-10-23 23:13:38 -0400

    Added/changed some stuff
    google command not actually working

--- cogs/default.py
+++ cogs/default.py
@@ -4,6 +4,7 @@ import random
 import json
 import aiohttp
 from os import remove
+import urllib
 
 with open('config/phrases.json') as phr:
     phrases = json.load(phr)
@@ -18,19 +19,19 @@ class General:
     @commands.command(name='ping',
                       description="A command that will send the bot's latency.")
     async def ping(self, ctx):
-        # `ping
+        # ping
         await ctx.send(f'{self.lambdabot.latency * 1000} ms')
 
     @commands.command(name='flip', aliases=['coinflip', 'coin_flip'],
                       description="A command that will randomly send either 'Heads' or Tails'.")
     async def flip(self, ctx):
-        # `flip
+        # flip
         await ctx.send(random.choice(("Heads", "Tails")))
 
     @commands.command(name='8ball', aliases=['eightball'],
                       description="A command that will send a random respond to a question.")
     async def ball(self, ctx, *question: str):
-        # `8ball <question>
+        # 8ball <question>
         ctx.trigger_typing()
         if question[-1][-1] != '?':
             await ctx.send("Is that really a question?")
@@ -40,23 +41,23 @@ class General:
     @commands.command(name='reverse',
                       description="A command that will reverse the given text")
     async def reverse(self, ctx, *, message):
-        # `reverse <message>
+        # reverse <message>
         await ctx.send(message[::-1])
 
     @commands.command(name='repeat',
                       description='A command that will repeat the text a number of times')
     async def repeat(self, ctx, amount: int, *, message):
-        # `repeat <times> <message>
+        # repeat <times> <message>
         if amount > 5:
             await ctx.send("That's too much. Use a number less than 5 please.")
         else:
             for i in range(amount):
                 await ctx.send(message)
 
-    @commands.command(name='gethelp',
+    @commands.command(name='gethelp', hidden=True,
                       description="A joke command that will send the National Suicide Prevention Line formatted with the given text.")
     async def gethelp(self, ctx, *, person: str):
-        # `gethelp <name>
+        # gethelp <name>
         person = person.title()
         await ctx.send(f"You're not alone **{person}**. Confidential help is available for free.\n"
                        "National Suicide Prevention Line\nCall **1-800-273-8255**\n"
@@ -65,8 +66,7 @@ class General:
     @commands.command(name='cat',
                       description="A command that will send a random cat photo from random.cat")
     async def cat(self, ctx):
-        # `cat
-        filename = None
+        # cat
         ctx.trigger_typing()
         async with aiohttp.ClientSession() as session:
             async with session.get('http://random.cat/meow') as r:
@@ -74,35 +74,44 @@ class General:
                     js = await r.json()
                     url = js['file']
                     filename = 'cat' + url[-4:]
-                    async with session.get(url) as resp2:
-                        test = await resp2.read()
+                    async with session.get(url) as r2:
+                        image = await r2.read()
                         with open(filename, 'wb') as catimg:
-                            catimg.write(test)
-        await ctx.send(file=discord.File(filename))
-        try:
-            remove(filename)
-        except Exception as e:
-            print(e)
+                            catimg.write(image)
+                    await ctx.send(file=discord.File(filename))
+                    try:
+                        remove(filename)
+                    except Exception as e:
+                        print(e)
+                else:
+                    await ctx.send(f"**ERROR**: Status == {r.status}")
 
     @commands.command(name='dog', description="A command that will send a random dog photo from shibe.online")
     async def dog(self, ctx):
-        # `dog
-        filename = None
+        # dog
         async with aiohttp.ClientSession() as session:
             async with session.get('http://shibe.online/api/shibes?count=1') as r:
                 if r.status == 200:
                     js = await r.json()
                     url = js[0]
                     filename = 'dog' + url[-4:]
-                    async with session.get(url) as resp2:
-                        test = await resp2.read()
+                    async with session.get(url) as r2:
+                        image = await r2.read()
                         with open(filename, 'wb') as dogimg:
-                            dogimg.write(test)
-        await ctx.send(file=discord.File(filename))
-        try:
-            remove(filename)
-        except Exception as e:
-            print(e)
+                            dogimg.write(image)
+                    await ctx.send(file=discord.File(filename))
+                    try:
+                        remove(filename)
+                    except Exception as e:
+                        print(e)
+                else:
+                    await ctx.send(f"**ERROR**: Status == {r.status}")
+
+    @commands.command(name='google', aliases=['g'], hidden=True,
+                      description="A command that will return a google query of what is inputted.")
+    async def google(self, ctx, *query: str):
+        # google <message>
+        pass
 
 
 def setup(lambdabot):
