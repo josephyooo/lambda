ba1a00b2f46abf489561392633861d4eacec8411
Polokniko <fizzyinc3@gmail.com>
2017-11-02 22:52:42 -0400

    update weather.py

--- cogs/weather.py
+++ cogs/weather.py
@@ -12,22 +12,23 @@ class Weather:
     def __init__(self, lambdabot):
         self.lambdabot = lambdabot
 
-    @commands.command(name="forecast",
-                      description="A command that will send the given location's weather forecast. unit parameter is optional")
+    @commands.command(name="forecast", aliases=['weather'],
+                      description="A command that will send the given location's weather forecast")
     async def forecast(self, ctx, *, location: str=''):
         # forecast <unit> <location>
         owm = pyowm.OWM(owm_api_key)
 
         observation = owm.weather_at_place(location)
         weather = observation.get_weather()
-        foundloc = observation.get_location().get_name()
+        lat = observation.get_location().get_lat()
+        lon = observation.get_location().get_lon()
         temp = weather.get_temperature('celsius')['temp']
-        windspd = str(weather.get_wind()['speed'])
+        wind = weather.get_wind()
 
-        await ctx.send('**Found Location:** {}'.format(foundloc))
+        await ctx.send('**Latitude:** {}, **Longitude:** {}'.format(lat, lon))
         await ctx.send('**Temperature:** {} celsius'.format(str(temp) + u'\N{DEGREE SIGN}'))
         await ctx.send('**Humidity:** {}'.format(str(weather.get_humidity()) + '%'))
-        await ctx.send('**Wind Speed:** {}'.format(windspd + ' m/s'))
+        await ctx.send('**Wind Speed:** {} at {}'.format(str(wind['speed']) + ' m/s', str(wind['deg']) + u'\N{DEGREE SIGN}'))
         await ctx.send('**Detailed Status:** {}'.format(weather.get_detailed_status()))
 
 
